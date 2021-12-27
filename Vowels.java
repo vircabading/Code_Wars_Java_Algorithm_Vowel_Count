ff682d73ae17ad7041d24c178e280fab509592ac
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-27 12:10:22 -0600

    add if statement looking for vowels

--- Vowels.java
+++ Vowels.java
@@ -6,13 +6,12 @@ public class Vowels {
 
     public static int getCount(String str) {
         int vowelsCount = 0;
-        
         for (int idx=0; idx < str.length(); idx++) {
-            if (    str[idx].equals("a") || 
-                    str[idx].equals("e") ||  
-                    str[idx].equals("i") ||
-                    str[idx].equals("o") ||
-                    str[idx].equals("u")) {
+            if (    Character.toString(str.charAt(idx)).equals("a") ||
+                    Character.toString(str.charAt(idx)).equals("e") ||
+                    Character.toString(str.charAt(idx)).equals("i") ||
+                    Character.toString(str.charAt(idx)).equals("o") ||
+                    Character.toString(str.charAt(idx)).equals("u") ) {
                 vowelsCount++;
             }
         }
--- VowelsTest.java
+++ VowelsTest.java
@@ -6,7 +6,7 @@
 public class VowelsTest {
   
     public static void main(String[] args) {
-        System.out.println(Vowels.getCount("abracadabra"));
+        System.out.println(Vowels.getCount("lkjlkjlkjkaeioukldkjvdkj"));
     }
     
 }
