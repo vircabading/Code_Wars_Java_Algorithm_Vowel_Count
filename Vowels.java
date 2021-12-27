37be8a962c2fa42fee0f63b37101cb6989b54beb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-27 12:16:53 -0600

    add tests

--- Vowels.java
+++ Vowels.java
@@ -5,16 +5,6 @@
 public class Vowels {
 
     public static int getCount(String str) {
-        int vowelsCount = 0;
-        for (int idx=0; idx < str.length(); idx++) {
-            if (    Character.toString(str.charAt(idx)).equals("a") ||
-                    Character.toString(str.charAt(idx)).equals("e") ||
-                    Character.toString(str.charAt(idx)).equals("i") ||
-                    Character.toString(str.charAt(idx)).equals("o") ||
-                    Character.toString(str.charAt(idx)).equals("u") ) {
-                vowelsCount++;
-            }
-        }
-        return vowelsCount;
+ 
     }  
 }
