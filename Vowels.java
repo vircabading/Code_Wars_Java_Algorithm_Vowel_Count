2bfea1500528da4b910bffdccdd4e8b23073ce11
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-27 11:35:40 -0600

    create isVowel method in Vowels class

--- Vowels.java
+++ Vowels.java
@@ -8,10 +8,19 @@ public class Vowels {
         int vowelsCount = 0;
         
         for (int idx=0; idx < str.length(); idx++) {
-            
+            if ( isVowel(str[idx]) ) {
+                vowelsCount++;
+            }
         }
 
         return vowelsCount;
+    }
+
+    public static boolean isVowel(String char) {
+        if ( (char.equals("a") || (char.equals("e") || (char.equals("i") || (char.equals("o") || (char.equals("u") ) {
+            return true;
+        }
+        return false;
     }
   
 }
